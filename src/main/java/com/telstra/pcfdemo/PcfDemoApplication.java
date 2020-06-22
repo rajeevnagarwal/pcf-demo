4375e851b6000bb82024f08c5ae5f12d8cd7b75d
H102639 <rajeev.nagarwal@team.telstra.com>
2020-06-22 11:53:36 +0530

    Initial Commit

---
+++ src/main/java/com/telstra/pcfdemo/PcfDemoApplication.java
@@ -0,0 +1,13 @@
+package com.telstra.pcfdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcfDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcfDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telstra/pcfdemo/PcfDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telstra.pcfdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcfDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
